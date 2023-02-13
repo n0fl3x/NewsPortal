1d61e207a3c93fbf6c0478ed164c1f950fc833e6
Roman Nevzorov <pomkanevzorov@gmail.com>
2023-02-14 02:19:46 +0600

    Completed D3.3.

--- NewsPaper/news/views.py
+++ NewsPaper/news/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.views.generic import ListView, DetailView
+from .models import Post
+from datetime import datetime
+
+
+class PostsList(ListView):
+    model = Post
+    ordering = '-date_of_creation'
+    template_name = 'posts.html'
+    context_object_name = 'posts'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
+
+
+class PostDetail(DetailView):
+    model = Post
+    template_name = 'post.html'
+    context_object_name = 'post'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
